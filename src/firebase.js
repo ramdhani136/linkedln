d4590c6d0b0ab5c44e654645e3e40fd8e2afdb18
Ilham Ramdhani <ramdhaniit@gmail.com>
2021-02-17 15:34:54 +0700

    install paket

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyZp5_b4QTgVS3FkF6JH33BuyEJ_ihRCY",
+  authDomain: "linkedin-clone-8e820.firebaseapp.com",
+  projectId: "linkedin-clone-8e820",
+  storageBucket: "linkedin-clone-8e820.appspot.com",
+  messagingSenderId: "767151624370",
+  appId: "1:767151624370:web:fbc4422fcfaff8f7d979ab",
+};
+
+
